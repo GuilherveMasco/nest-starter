6fac252eff8aa040105a0f2cb2a431ef1f35bb59
Guilherme Vasco <guilhermevasco08@gmail.com>
2023-04-18 16:58:16 -0300

    Edit movie controller

--- src/modules/domain/movie/movie.controller.ts
+++ src/modules/domain/movie/movie.controller.ts
@@ -8,7 +8,9 @@ export class MovieController {
     constructor(private readonly movieService: MovieService) {}
 
     @Post('create')
-    async create(@Body(new JoiPipe({ group: 'CREATE' })) CreateMovieDto: CreateMovieDto) {
+    async create(
+        @Body(new JoiPipe({ group: 'CREATE' })) CreateMovieDto: CreateMovieDto,
+    ) {
         return await this.movieService.createMovie(CreateMovieDto);
     }
 }
